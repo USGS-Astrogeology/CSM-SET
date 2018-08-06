e540dd9e76ed3e567f1bf9b9a2428b7ba6e3e02c
Kristin Berry <kberry@usgs.gov>
2018-08-06 15:35:44 -0700

    Call SensorUtil's phase angle and print output.

--- src/Set.cpp
+++ src/Set.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include "SensorUtils.h"
+
 using namespace std;
 
 int main() {
-   cout << "Hello World!" << endl;
-   return 0;
+  // Calculate the phase angle
+  std::vector<double> lookDir = {0.0, 0.0, 0.0};
+  std::vector<double> sunPosition = {0.0, 0.0, 0.0};
+  
+  double phaseAngle = PhaseAngle(lookDir, sunPosition); 
+  std::cout << "Phase Angle: " << phaseAngle << std::endl; 
+
+  return 0;
 }
