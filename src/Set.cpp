c1acc583b1033410b683131135901e13903d48a1
Kristin Berry <kberry@usgs.gov>
2018-08-06 16:13:59 -0700

    Read the ISD file into the csm::Isd object in the SET application

--- src/Set.cpp
+++ src/Set.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <fstream>
 
+#include "UsgsAstroFramePlugin.h"
+#include "UsgsAstroFrameSensorModel.h"
 #include "SensorUtils.h"
+
+#include <json/json.hpp>
+using json = nlohmann::json;
 
 using namespace std;
 
 int main() {
+  // Load ISD file
+  csm::Isd imageSupportData("simpleFramerISD.json");
+  imageSupportData.clearAllParams();
+
+  std::ifstream isdFile("simpleFramerISD.json");
+  json jsonIsd = json::parse(isdFile);
+
+  for (json::iterator it = jsonIsd.begin(); it != jsonIsd.end(); ++it) {
+    imageSupportData.addParam(it.key(), it.value().dump());
+  }
+  isdFile.close(); 
+  
+  // Check that csm::Isd was populated correctly: 
+  std::cout << "Focal length value: " << imageSupportData.param("focal_length") << std::endl;
+
   // Calculate the phase angle
   std::vector<double> lookDir = {0.0, 0.0, 0.0};
   std::vector<double> sunPosition = {0.0, 0.0, 0.0};
