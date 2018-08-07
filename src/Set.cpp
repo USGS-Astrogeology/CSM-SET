dd72a04622755554ae32c76ef7f3b72a83d9fa9d
Kristin Berry <kberry@usgs.gov>
2018-08-06 17:48:19 -0700

    Fix parsing of json, added instantiating a camera model

--- src/Set.cpp
+++ src/Set.cpp
@@ -5,6 +5,8 @@
 #include "UsgsAstroFrameSensorModel.h"
 #include "SensorUtils.h"
 
+#include <sstream>
+#include <string>
 #include <json/json.hpp>
 using json = nlohmann::json;
 
@@ -19,18 +21,37 @@ int main() {
   json jsonIsd = json::parse(isdFile);
 
   for (json::iterator it = jsonIsd.begin(); it != jsonIsd.end(); ++it) {
-    imageSupportData.addParam(it.key(), it.value().dump());
+    if (it.value().size() >1 ) {
+      vector<double> v = it.value();
+      for (int j=0;j < v.size(); j++) {
+        ostringstream val;
+        val << setprecision(15) << v[j];
+        imageSupportData.addParam(it.key(),val.str());
+      }
+    }
+    else {
+      imageSupportData.addParam(it.key(), it.value().dump());
+    }
   }
   isdFile.close(); 
-  
-  // Check that csm::Isd was populated correctly: 
-  std::cout << "Focal length value: " << imageSupportData.param("focal_length") << std::endl;
+
+  // Instantiate a Camera Model 
+  csm::WarningList *warnings;
+  UsgsAstroFramePlugin framePlugin;
+  csm::Model *cameraModel = framePlugin.constructModelFromISD(imageSupportData,
+                                                             "USGS_ASTRO_FRAME_SENSOR_MODEL",
+                                                              warnings);
+  // Grab the Camera Model model state to get the sun location
+  std::string modelState = cameraModel->getModelState(); 
+  json parsedIsd = json::parse(modelState);
+
+  // TODO: actually use the values from the CSM camera model for the phase angle calculation
 
   // Calculate the phase angle
   std::vector<double> lookDir = {0.0, 0.0, 0.0};
   std::vector<double> sunPosition = {0.0, 0.0, 0.0};
   
-  double phaseAngle = PhaseAngle(lookDir, sunPosition); 
+  double phaseAngle = PhaseAngle(lookDir, sunPosition);
   std::cout << "Phase Angle: " << phaseAngle << std::endl; 
 
   return 0;
